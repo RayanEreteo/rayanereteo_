ea17140efc31b96bdca10dbac28a68498c21e7c6
RayanEreteo <rayabf5@gmail.com>
2026-02-20 21:03:28 +0100

    Initial Commit
    Added navbar first version

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <nav>
+        <div className="nes-container with-title is-centered" id='navbar'>
+          <p className='title'>Menu Principal</p>
+          <h1 className='nes-text'>Rayan Ereteo</h1>
+          <div className="btns-container">
+            <a href="" className='nes-btn'>Mes Quêtes</a>
+            <a href="" className='nes-btn'>Mes Compétences</a>
+          </div>
+        </div>
+      </nav>
+    </main>
+  )
+}
+
+export default App
