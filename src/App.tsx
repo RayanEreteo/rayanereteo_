f9118e9b0da5fc48af879c6801482d554024d05a
RayanEreteo <rayabf5@gmail.com>
2026-02-21 17:28:00 +0100

    Added socials

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,10 @@ function App() {
         <h2 className='nes-text'>Qui suis-je ?</h2>
         <br />
         <p>Mon nom est <span className='nes-text is-primary'>Rayan</span>, Je suis un développeur passionné</p>
+        <div className='nes-container' id="socials">
+            <a href="https://github.com/RayanEreteo" target='_blank'><i className="nes-icon github is-medium"></i></a>
+            <a href="https://github.com/RayanEreteo" target='_blank'><i className="nes-icon linkedin is-medium"></i></a>
+        </div>
       </div>
     </main>
   )
