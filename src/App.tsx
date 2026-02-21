2ca248450a209fa724d9c932a91bbe82044206f9
RayanEreteo <rayabf5@gmail.com>
2026-02-21 17:31:16 +0100

    update linkedin link

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
         <p>Mon nom est <span className='nes-text is-primary'>Rayan</span>, Je suis un développeur passionné</p>
         <div className='nes-container' id="socials">
             <a href="https://github.com/RayanEreteo" target='_blank'><i className="nes-icon github is-medium"></i></a>
-            <a href="https://github.com/RayanEreteo" target='_blank'><i className="nes-icon linkedin is-medium"></i></a>
+            <a href="https://www.linkedin.com/in/rayan-ereteo-65099437a/" target='_blank'><i className="nes-icon linkedin is-medium"></i></a>
         </div>
       </div>
     </main>
