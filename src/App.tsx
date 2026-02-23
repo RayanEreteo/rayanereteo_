b59af81bae46cdf7620e3d3bef3f5fe00cce05ce
RayanEreteo <rayabf5@gmail.com>
2026-02-23 14:51:51 +0100

    Enhanced text and added projects section

--- src/App.tsx
+++ src/App.tsx
@@ -27,10 +27,16 @@ function App() {
       <div className='mt' id='presentation'>
         <h2 className='nes-text'>Qui suis-je ?</h2>
         <br />
-        <p>Mon nom est <span className='nes-text is-primary'>Rayan</span>, Je suis un développeur passionné</p>
+        <p id='presentation-text'>Mon nom est <span className='nes-text is-primary'>Rayan</span>, Je suis un développeur passionné spécialisé en développement web. J'aime créer des expériences web intuitives et performantes.</p>
         <div className='nes-container' id="socials">
             <a href="https://github.com/RayanEreteo" target='_blank'><i className="nes-icon github is-medium"></i></a>
             <a href="https://www.linkedin.com/in/rayan-ereteo-65099437a/" target='_blank'><i className="nes-icon linkedin is-medium"></i></a>
+        </div>
+      </div>
+      <div className='mt' id='projects'>
+        <h2 className='nes-text'>Quêtes principales</h2>
+        <div className="nes-container">
+          {/* Project Component here... */}
         </div>
       </div>
     </main>
