9d497e7791b39977b0fa8bed8b851a3d5ccfc2f3
RayanEreteo <rayabf5@gmail.com>
2026-02-23 17:04:45 +0100

    Added "skills" section

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
   const [downloadCVText, setDownloadCVText] = useState<String>("Fiche Personnage")
 
-  function downloadCV(){
+  function downloadCV() {
     const elem = document.createElement('a');
     elem.href = '/CV_Développeur.pdf';
     elem.target = '_blank';
@@ -29,8 +29,36 @@ function App() {
         <br />
         <p id='presentation-text'>Mon nom est <span className='nes-text is-primary'>Rayan</span>, Je suis un développeur passionné spécialisé en développement web. J'aime créer des expériences web intuitives et performantes.</p>
         <div className='nes-container' id="socials">
-            <a href="https://github.com/RayanEreteo" target='_blank'><i className="nes-icon github is-medium"></i></a>
-            <a href="https://www.linkedin.com/in/rayan-ereteo-65099437a/" target='_blank'><i className="nes-icon linkedin is-medium"></i></a>
+          <a href="https://github.com/RayanEreteo" target='_blank'><i className="nes-icon github is-medium"></i></a>
+          <a href="https://www.linkedin.com/in/rayan-ereteo-65099437a/" target='_blank'><i className="nes-icon linkedin is-medium"></i></a>
+        </div>
+      </div>
+      <div className="mt" id='skills'>
+        <h2 className='nes-text'>Compétences</h2>
+        <div className="skills-grid">
+          <div className="nes-container with-title">
+            <p className="title">Front-end</p>
+            <ul className='nes-list is-circle' id='frontend-list'>
+              <li>Javascript / Typescript</li>
+              <li>NextJS</li>
+              <li>React</li>
+              <li>C</li>
+              <li>Tailwind</li>
+            </ul>
+          </div>
+
+          <div className="nes-container with-title">
+            <p className="title">Back-end & Outils</p>
+            <ul className="nes-list is-disc">
+              <li>Node.js / Express</li>
+              <li>PHP</li>
+              <li>Python / Flask</li>
+              <li>MySQL / MongoDB</li>
+              <li>Git</li>
+              <li>Windows</li>
+              <li>Linux</li>
+            </ul>
+          </div>
         </div>
       </div>
       <div className='mt' id='projects'>
