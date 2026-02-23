44414e2b62fab0fb8f69f248c1ca198427612af5
RayanEreteo <rayabf5@gmail.com>
2026-02-23 20:58:37 +0100

    Added project component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import './App.css'
+import Project from './components/Project';
+
+import willitdryT from "./assets/willitdry.png"
+import livePictureT from "./assets/livepictures.png"
 
 function App() {
   const [downloadCVText, setDownloadCVText] = useState<String>("Fiche Personnage")
@@ -64,7 +68,8 @@ function App() {
       <div className='mt' id='projects'>
         <h2 className='nes-text'>Quêtes principales</h2>
         <div className="nes-container">
-          {/* Project Component here... */}
+          <Project title='Will It Dry ?' description="”Will It Dry ?” est une application utilisant l'API d'openweathermap et utilise les données reçus pour prédire si la météo actuelle est dans une bonne condition pour tendre le linge." thumbnail={willitdryT} projectURL='https://github.com/RayanEreteo/WillItDry'></Project>
+          <Project title='Live Pictures' description="”Live Pictures” est une application permettant d'envoyer en temps réel des images a toutes personnes présentes dans un canal." thumbnail={livePictureT} projectURL='https://github.com/RayanEreteo/LivePictures'></Project>
         </div>
       </div>
     </main>
